97862f3d818c072f2d7e0896b215041ebde8d5d3
Stefan Marr <git@stefan-marr.de>
2021-09-02 20:12:06 +0100

    Added OptCarrot benchmark
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ awfy/optcarrot.rb
@@ -0,0 +1,31 @@
+$LOAD_PATH.unshift "#{__dir__}/../../bench/optcarrot"
+require 'lib/optcarrot'
+
+class OptCarrot < Benchmark
+
+  def initialize
+    @rom = "#{__dir__}/../../bench/optcarrot/examples/Lan_Master.nes"
+    @iteration = 0
+  end
+
+  def benchmark
+    @nes.step
+    @iteration += 1
+  end
+
+  def inner_benchmark_loop(inner_iterations)
+    @nes = Optcarrot::NES.new ['--headless', @rom]
+    @nes.reset
+    @iteration = 0
+
+    inner_iterations.times do
+      return false unless verify_result(benchmark)
+    end
+    true
+  end
+
+  def verify_result(result)
+    frame = @nes.instance_variable_get :@frame
+    result == frame
+  end
+end
