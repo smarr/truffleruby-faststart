46f4b50dc36699c57e3d55855809426df8df6b12
Stefan Marr <git@stefan-marr.de>
2021-09-02 23:35:03 +0100

    Added AsciiDoctor benchmarks
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ awfy/asciidoctor-convert.rb
@@ -0,0 +1,18 @@
+require_relative '../../bench/asciidoctor/asciidoctor/lib/asciidoctor'
+
+class AsciidoctorConvert < Benchmark
+  def initialize
+    sample_file = "#{__dir__}/../../bench/asciidoctor/data/userguide.adoc"
+    @sample_file_text = File.read(sample_file)
+    @result = File.read("#{__dir__}/asciidoctor-convert.result.html")
+  end
+
+  def benchmark
+    document = Asciidoctor.load(@sample_file_text)
+    document.convert
+  end
+
+  def verify_result(result)
+    @result == result
+  end
+end
---
+++ awfy/asciidoctor-load-file.rb
@@ -0,0 +1,16 @@
+require_relative '../../bench/asciidoctor/asciidoctor/lib/asciidoctor'
+
+class AsciidoctorLoadFile < Benchmark
+  def initialize
+    sample_file = "#{__dir__}/../../bench/asciidoctor/data/userguide.adoc"
+    @sample_file_text = File.read(sample_file)
+  end
+
+  def benchmark
+    Asciidoctor.load(@sample_file_text)
+  end
+
+  def verify_result(result)
+    result.doctitle == "AsciiDoc User Guide" and result.blocks.size == 47 and result.blocks[40].title == "AsciiDoc Safe Mode"
+  end
+end
