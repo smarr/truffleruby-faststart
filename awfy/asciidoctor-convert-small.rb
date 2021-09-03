69e4da82a6459f2567a7fc888abd4ccb0c750907
Stefan Marr <git@stefan-marr.de>
2021-09-03 11:15:42 +0100

    Resize benchmarks
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ awfy/asciidoctor-convert-small.rb
@@ -0,0 +1,18 @@
+require_relative '../../bench/asciidoctor/asciidoctor/lib/asciidoctor'
+
+class AsciidoctorConvertSmall < Benchmark
+  def initialize
+    sample_file = "#{__dir__}/asciidoctor-small.adoc"
+    @sample_file_text = File.read(sample_file)
+    @result = File.read("#{__dir__}/asciidoctor-small.html")
+  end
+
+  def benchmark
+    document = Asciidoctor.load(@sample_file_text)
+    document.convert
+  end
+
+  def verify_result(result)
+    @result == result
+  end
+end
---
+++ awfy/asciidoctor-load-file-small.rb
@@ -0,0 +1,16 @@
+require_relative '../../bench/asciidoctor/asciidoctor/lib/asciidoctor'
+
+class AsciidoctorLoadFileSmall < Benchmark
+  def initialize
+    sample_file = "#{__dir__}/asciidoctor-small.adoc"
+    @sample_file_text = File.read(sample_file)
+  end
+
+  def benchmark
+    Asciidoctor.load(@sample_file_text)
+  end
+
+  def verify_result(result)
+    result.doctitle == "AsciiDoc User Guide" and result.blocks.size == 11 and result.blocks[9].title == "Document Processing"
+  end
+end
