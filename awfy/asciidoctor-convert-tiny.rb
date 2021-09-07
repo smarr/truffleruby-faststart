b906516003d0a9bf38d815296a62f9bb5e3a438b
Stefan Marr <git@stefan-marr.de>
2021-09-07 21:54:21 +0100

    Added tiny workload to reduce run time of AsciiDoctor benchmark
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ awfy/asciidoctor-convert-tiny.rb
@@ -0,0 +1,18 @@
+require_relative '../asciidoctor/asciidoctor/lib/asciidoctor'
+
+class AsciidoctorConvertTiny < Benchmark
+  def initialize
+    sample_file = "#{__dir__}/asciidoctor-tiny.adoc"
+    @sample_file_text = File.read(sample_file)
+    @expected_result = File.read("#{__dir__}/asciidoctor-tiny.html")
+  end
+
+  def benchmark
+    document = Asciidoctor.load(@sample_file_text)
+    document.convert
+  end
+
+  def verify_result(result)
+    @expected_result == result
+  end
+end
---
+++ awfy/asciidoctor-load-file-tiny.rb
@@ -0,0 +1,16 @@
+require_relative '../asciidoctor/asciidoctor/lib/asciidoctor'
+
+class AsciidoctorLoadFileTiny < Benchmark
+  def initialize
+    sample_file = "#{__dir__}/asciidoctor-tiny.adoc"
+    @sample_file_text = File.read(sample_file)
+  end
+
+  def benchmark
+    Asciidoctor.load(@sample_file_text)
+  end
+
+  def verify_result(result)
+    result.doctitle == "AsciiDoc User Guide" and result.blocks.size == 8 and result.blocks[7].title == "HTML5 and XHTML 1.1"
+  end
+end
