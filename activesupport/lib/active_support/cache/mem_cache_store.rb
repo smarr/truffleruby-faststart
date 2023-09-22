5f50a68b56739bcff1e3b07e25a70d06179e786c
Stefan Marr <git@stefan-marr.de>
2023-09-22 13:02:55 +0100

    Disable one more warning

--- activesupport/lib/active_support/cache/mem_cache_store.rb
+++ activesupport/lib/active_support/cache/mem_cache_store.rb
@@ -3,7 +3,7 @@
 begin
   require "dalli"
 rescue LoadError => e
-  warn "You don't have dalli installed in your application. Please add it to your Gemfile and run bundle install"
+  # warn "You don't have dalli installed in your application. Please add it to your Gemfile and run bundle install"
   raise e
 end
 
